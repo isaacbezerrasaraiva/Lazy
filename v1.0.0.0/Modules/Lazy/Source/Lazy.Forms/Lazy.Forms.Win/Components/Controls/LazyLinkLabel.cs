2f6aac5cf19c90506a004ea5db2db922c1c3b0f4
Isaac Saraiva <isaacbezerrasaraiva@outlook.com>
2021-06-14 23:08:20 -0300

    1. Added some new controls

---
+++ v1.0.0.0/Modules/Lazy/Source/Lazy.Forms/Lazy.Forms.Win/Components/Controls/LazyLinkLabel.cs
@@ -0,0 +1,77 @@
+﻿// LazyLinkLabel.cs
+//
+// This file is integrated part of Ark project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2021, June 14
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Text;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace Lazy.Forms.Win
+{
+    public class LazyLinkLabel : LinkLabel
+    {
+        #region Variables
+
+        private Color linkColorTemp;
+
+        #endregion Variables
+
+        #region Constructors
+
+        public LazyLinkLabel()
+        {
+            this.linkColorTemp = this.LinkColor;
+            this.LinkHover = this.LinkColor;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+
+            if (this.AutoOpenUrl == true)
+                Process.Start(new ProcessStartInfo(this.Url) { UseShellExecute = true });
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+
+            this.linkColorTemp = this.LinkColor;
+            this.LinkColor = this.LinkHover;
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+
+            this.LinkColor = this.linkColorTemp;
+        }
+
+        #endregion Methods
+
+        #region Properties
+
+        public Color LinkHover { get; set; }
+
+        public Boolean AutoOpenUrl { get; set; }
+
+        public String Url { get; set; }
+
+        #endregion Properties
+    }
+}
---
+++ v1.0.0.0/Modules/Lazy/Source/Lazy.Forms/Lazy.Forms.Win/Components/Controls/LazyPictureBox.cs
@@ -0,0 +1,113 @@
+﻿// LazyPictureBox.cs
+//
+// This file is integrated part of Ark project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2021, June 14
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Text;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Drawing.Drawing2D;
+using System.Collections.Generic;
+
+namespace Lazy.Forms.Win
+{
+    public class LazyPictureBox : PictureBox
+    {
+        #region Variables
+
+        private Int32 lastWidth;
+        private Int32 lastHeight;
+        private Int32 lastLocationX;
+        private Int32 lastLocationY;
+        private Boolean drawAsCircle;
+
+        #endregion Variables
+
+        #region Constructors
+
+        public LazyPictureBox()
+        {
+            this.drawAsCircle = false;
+
+            this.lastWidth = 0;
+            this.lastHeight = 0;
+            this.lastLocationX = this.Location.X;
+            this.lastLocationY = this.Location.Y;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            if (this.drawAsCircle == true)
+            {
+                if (this.Location.X != this.lastLocationX)
+                {
+                    this.Height = this.Width;
+                }
+                else if (this.Location.Y != this.lastLocationY)
+                {
+                    this.Width = this.Height;
+                }
+                else
+                {
+                    if (this.Width != this.lastWidth)
+                        this.Height = this.Width;
+
+                    if (this.Height != this.lastHeight)
+                        this.Width = this.Height;
+                }
+
+                this.lastWidth = this.Width;
+                this.lastHeight = this.Height;
+                this.lastLocationX = this.Location.X;
+                this.lastLocationY = this.Location.Y;
+
+                GraphicsPath graphicsPath = new GraphicsPath();
+                graphicsPath.AddEllipse(0, 0, this.Width - 2, this.Height - 2);
+                this.Region = new Region(graphicsPath);
+            }
+        }
+
+        #endregion Methods
+
+        #region Properties
+
+        public Boolean DrawAsCircle
+        {
+            get { return this.drawAsCircle; }
+            set 
+            {
+                this.drawAsCircle = value;
+                
+                if (this.drawAsCircle == true)
+                {
+                    OnResize(new EventArgs());
+                }
+                else
+                {
+                    this.lastWidth = 0;
+                    this.lastHeight = 0;
+                    this.lastLocationX = this.Location.X;
+                    this.lastLocationY = this.Location.Y;
+
+                    this.Region = null;
+                }
+            }
+        }
+
+        #endregion Properties
+    }
+}
